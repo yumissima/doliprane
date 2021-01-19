965ab332e8faea560213eb3eb98e65eefaa6515b
yumissima <pascalelaunois@gmail.com>
2021-01-19 16:40:27 +0100

    test1

--- lib/mulptiples.rb
+++ lib/mulptiples.rb
@@ -1,24 +1,34 @@
-
-
 def is_multiple_of_3_or_5?(current_number)
-    return 
-    if 
-
+    if current_number % 3 == 0 || current_number % 5 == 0
+      return true
+    else
+      return false
+    end
 end
 
 
+# def sum_of_3_or_5_multiples(final_number)
+
+# end
 
 
+# def is_multiple_of_3_or_5?(n)
+
+# end
 
 def sum_of_3_or_5_multiples(final_number)
-
-end
-
-
-
-
-
-
-def is_multiple_of_3_or_5?(n)
-
-end
+  
+    final_sum=0
+    
+      if final_number is_i?(Fixnum) && final_number >= 0
+  
+        0.upto(final_number-1) do |current_number|
+            if is_multiple_of_3_or_5? (current_number) == true 
+              final_sum = final_sum + current_number
+            else 
+            end 
+        end
+        return "Yo ! Je ne prends que les entiers naturels. TG"
+      end
+    return final_sum
+  end 
--- spec/multiples_spec.rb
+++ spec/multiples_spec.rb
@@ -7,21 +7,24 @@ describe "the is_multiple_of_3_or_5? method" do
     end
   
     it "should return FALSE when an integer is NOT a multiple of 3 or 5" do
-        expect(is_not_multiple_of_3_or_5?(3)).to eq(true)
-        expect(is_not_multiple_of_3_or_5?(5)).to eq(true)
-        expect(is_not_multiple_of_3_or_5?(51)).to eq(true)
-        expect(is_not_multiple_of_3_or_5?(45)).to eq(true) 
+        expect(is_not_multiple_of_3_or_5?(2)).to eq(false)
+        expect(is_not_multiple_of_3_or_5?(7)).to eq(false)
+        expect(is_not_multiple_of_3_or_5?(64)).to eq(false)
+        
+    end   
       # je te laisse écrire ces tests-là
+end
 
 
-
-
-
-describe de sum_of_3_or_5_multiples
-
-
-
-
-
+describe  "the sum of 3 or 5 multiples method" do
+    it "should return a result" do
+        expect(is_not_multiple_of_3_or_5?(10)).to eq(23)
+        expect(is_not_multiple_of_3_or_5?(11)).to eq(33)
+        expect(is_not_multiple_of_3_or_5?(0 || 3)).to eq(0)
+        expect(is_not_multiple_of_3_or_5?(-1, 1.23, "chiffre")).to eq("Yo ! Je ne prends que les entiers naturels. TG")
     end
-  end
+        it "should deny the user entry method" do
+        expect()
+
+
+end    
